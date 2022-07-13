9fc30db971ae6ab7986ab6337bcf122d61073c66
ilkin1zahidov <ilkiniz@code.edu.az>
2022-07-14 02:06:20 +0400

    List

---
+++ Models/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic.Models
+{
+    public class MyList
+    {
+      
+      
+        public wish[] arr;
+        public MyList()
+        {
+            arr = new wish[0];
+        }
+        public void add(wish wishlist )
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+               arr[arr.Length - 1] = wishlist;
+        }
+        public void getall()
+        {
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item.Id + "-" + item.Name);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Generic.Models;
+using System;
+
+namespace Generic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList myList = new MyList();
+           
+
+            wish wishlist = new wish
+            {
+                Id = 1,
+                Name = "Bmw"
+
+            };
+           
+            myList.getall();
+
+            wish wishlist2 = new wish
+            {
+                Id = 2,
+                Name = "Mercedes"
+            };
+
+            myList.add(wishlist);
+            myList.add(wishlist2);
+
+
+
+            myList.getall();
+
+        }
+    }
+   
+    public class wish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
